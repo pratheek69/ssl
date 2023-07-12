dbf63927b838f12ce84a691db8be5443b9ce611d
Pratheek Shetty <79799243+pratheekshetty21@users.noreply.github.com>
2023-07-12 11:37:53 +0530

    Add files via upload

---
+++ f2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    int y = 10;
+
+    int sum = x + y;
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
+
---
+++ src.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    /* This is a block comment */
+    int a = 10;
+    // This is a single-line comment
+    printf("Value of a: %d\n", a);
+
+    return 0;
+}
+
